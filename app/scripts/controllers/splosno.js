46aabec924baeee8449db89529ba8e6935e8c48d
mancazerovnik <manca.zerovnik3@gmail.com>
2015-12-18 16:50:20 +0100

    splosni podatki - spol, starost, kraj bivanja

--- app/scripts/controllers/splosno.js
+++ app/scripts/controllers/splosno.js
@@ -8,10 +8,102 @@
  * Controller of the modooApp
  */
 angular.module('modooApp')
-  .controller('SplosnoCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SplosnoCtrl', function ($scope, $http) {
+        $http.get('../../assets/data/data.json').success(function(data) {
+            $scope.mainInfo = data;  
+            var genderNUM = _.countBy($scope.mainInfo, function(x) {
+            return x.spol == 'M' ? 'm': 'z';
+            });  
+            var livingNUM = _.countBy($scope.mainInfo, function(x) {
+            return x.kraj_bivanja == 'v mestu' ? 'mesto': 'podezelje';
+            });  
+            var ageNUM = _.countBy($scope.mainInfo, function(x) {
+                return String(x.starost).charAt(0);
+            });  
+            update(genderNUM, livingNUM, ageNUM);
+   
+        });
+        
+        function update(genderNUM, livingNUM, ageNUM){
+            $scope.optionsGender = {
+                chart: {
+                    type: 'pieChart',
+                    height: 500,
+                    x: function(d){return d.key;},
+                    y: function(d){return d.y;},
+                    labelSunbeamLayout: true
+                }
+            };
+            $scope.dataGender = [
+                {
+                    key: "Ženske",
+                    y: genderNUM.z
+                },
+                {
+                    key: "Moški",
+                    y: genderNUM.m
+                }
+            ];
+
+            $scope.optionsPlace = {
+                chart: {
+                    type: 'pieChart',
+                    height: 500,
+                    x: function(d){return d.key;},
+                    y: function(d){return d.y;},
+                    labelSunbeamLayout: true
+                }
+            };
+            $scope.dataPlace = [
+                {
+                    key: "Podeželje",
+                    y: livingNUM.podezelje
+                },
+                {
+                    key: "Mesto",
+                    y: livingNUM.mesto
+                }
+            ];
+
+
+            $scope.optionsAge = {
+                chart: {
+                    type: 'discreteBarChart',
+                    height: 450,
+
+                    x: function(d){return d.key;},
+                    y: function(d){return d.y;},
+                    showValues: true,
+                    valueFormat: function(d){
+                        return d3.format(',.0f')(d);
+                    },
+                    duration: 500,
+                    xAxis: {
+                        axisLabel: 'Starost v letih'
+                    },
+                    yAxis: {
+                        axisLabel: 'Število ljudi',
+                    }
+                }
+            };
+
+            $scope.dataAge = [];
+            $scope.dataAge[0] = 
+            {
+                key: "Cumulative Return",
+                values: []
+            }
+
+            for (var property in ageNUM) {
+                console.log(property);
+                console.log(ageNUM[property]);
+                $scope.dataAge[0].values.push({
+                    key: property + "0 do " + property + "9",
+                    y: ageNUM[property]
+               });
+            }
+            
+            
+                
+        }
   });
