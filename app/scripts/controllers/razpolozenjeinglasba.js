6c35c75021e707d08b02eb2f0f7fabe71f54193d
mancazerovnik <manca.zerovnik3@gmail.com>
2015-12-18 14:16:00 +0100

    sort by songs

--- app/scripts/controllers/razpolozenjeinglasba.js
+++ app/scripts/controllers/razpolozenjeinglasba.js
@@ -11,28 +11,43 @@ angular.module('modooApp')
   .controller('RazpolozenjeInGlasbaCtrl', function ($scope, $http, $window) {
     $scope.mainInfo = null;
     $scope.filter = {
-        "male": false, 
+        "male": true, 
         "female": false,
         "agemin": 5,
         "agemax": 100,
-        "city": false,
+        "city": true,
         "domestic": false,
         "schoolmin": 0,
         "schoolmax": 20,
         "activeinmusicmin": 0,
         "activeinmusicmax": 20,
-        "onehour": false,
+        "onehour": true,
         "twohour": false,
         "threehour": false,
-        "fourhour": false
+        "fourhour": false,
+        "song": 0
     };
 
     $http.get('../../assets/data/data.json').success(function(data) {
         $scope.mainInfo = data;
+        $scope.songs = _.uniq(_.sortBy(_.flatten(
+                            _.map($scope.mainInfo, function(num){ 
+                                return _.map(num.pesmi, function(x) {
+                                                            return x.pesem_id;
+                                                        }
+                                        );
+                                }
+                            )
+                       ), function(x) { return x; }), true);
+        $scope.filter.song = $scope.songs[0];
+        $scope.update();          
     });
+
+    
     $scope.update = function () {
-        console.log($scope.filter);
-        $scope.filteredData = _.filter($scope.mainInfo, function(num){ 
+
+        $scope.filteredData = _.filter(_.flatten(_.map(_.filter($scope.mainInfo, function(num){ 
+            
             return (($scope.filter.male && num.spol == "M" ||
             $scope.filter.female && num.spol == "Z")
             && (($scope.filter.schoolmin <= parseInt(num.glasbena_sola) && 
@@ -47,17 +62,12 @@ angular.module('modooApp')
                 ($scope.filter.twohour && num.poslusanje_glasbe == "2") ||
                 ($scope.filter.threehour && num.poslusanje_glasbe == "3") ||
                 ($scope.filter.fourhour && num.poslusanje_glasbe == "4"))
-            );
+            );}), function(x) {return x.pesmi; })), function(x) { return x.pesem_id === $scope.filter.song; });
             
-        });
-    }
-    //$scope.$watch("#age", $scope.update);
+        if(!$scope.$$phase) {
+          $scope.$apply();
+        }
 
-    //var slider = $('#age').slider()
-      //  .on('slide', $scope.update())
-        //.data('slider');
-
-    //var slider = new Slider('#age', {});
-    //var slider = new Slider('#music_school', {});
-    //var slider = new Slider('#involvement_in_music', {});
+        
+    };
   });
