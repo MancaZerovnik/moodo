21b3ae93f64da6d7056e6849ddcae5e234d8df8d
PrimozGodec <p.godec9@gmail.com>
2015-12-20 13:37:51 +0100

    Adde vzbujena in izrazena razpolozenja to the third page.

--- app/scripts/controllers/razpolozenjeinglasba.js
+++ app/scripts/controllers/razpolozenjeinglasba.js
@@ -12,19 +12,19 @@ angular.module('modooApp')
     $scope.mainInfo = null;
     $scope.filter = {
         "male": true, 
-        "female": false,
+        "female": true,
         "agemin": 5,
         "agemax": 100,
         "city": true,
-        "domestic": false,
+        "domestic": true,
         "schoolmin": 0,
         "schoolmax": 20,
         "activeinmusicmin": 0,
         "activeinmusicmax": 20,
         "onehour": true,
-        "twohour": false,
-        "threehour": false,
-        "fourhour": false,
+        "twohour": true,
+        "threehour": true,
+        "fourhour": true,
         "song": 0
     };
 
@@ -75,6 +75,79 @@ angular.module('modooApp')
           $scope.$apply();
         }
 
+        $scope.vzbujenaData = getMoodVAEstimationData($scope.filteredData, 'vzbujena_custva');
+        $scope.izrazenaData = getMoodVAEstimationData($scope.filteredData, 'izrazena_custva');
+
         
     };
+
+    $scope.viCustvaGraph = setVAgraphLegend();
+
+    function setVAgraphLegend()
+    {
+        return {
+            chart: {
+                type: 'scatterChart',
+                height: 450,
+                color: d3.scale.category10().range(),
+                scatter: {
+                    onlyCircles: false
+                },
+                tooltipContent: function(key) {
+                    return '<h3>' + key + '</h3>';
+                },
+                duration: 350,
+                xAxis: {
+                    axisLabel: 'Valence',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    }
+                },
+                xDomain: [-1, 1],
+                yDomain: [-1,1],
+                yAxis: {
+                    axisLabel: 'Arousal',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: -5
+                },
+                showLegend: true
+            }
+        };
+    }
+
+    function getMoodVAEstimationData(inputData, data_key)
+    {
+        var data = [];
+        if(inputData) {          
+            for (var i = 0; i < inputData.length; i++)
+            {                
+                for (var j = 0; j < inputData[i][data_key].length; j++)
+                {
+                    if (getDictonaryIdxByKey(data, inputData[i][data_key][j]['ime']) === null
+                        && 'x' in inputData[i][data_key][j])
+                        data.push({key: inputData[i][data_key][j]['ime'], values:[]});
+                    
+                        if ('x' in inputData[i][data_key][j])
+                            data[getDictonaryIdxByKey(data, inputData[i][data_key][j]['ime'])]['values'].push({
+                                x: inputData[i][data_key][j]['x'],
+                                y: inputData[i][data_key][j]['y']
+                            });
+                }                
+            }
+        }
+        return data;
+    }
+
+    function getDictonaryIdxByKey(l, kvalue)
+    {
+        for(var i = 0; i < l.length; i++)
+            if(l[i].key === kvalue)
+                return i;
+
+        return null;
+    }
+
+
   });
