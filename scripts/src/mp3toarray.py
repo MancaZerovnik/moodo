957ec2979c9ba216375b5a8b79ee2be801331fff
PrimozGodec <p.godec9@gmail.com>
2016-01-21 11:27:52 +0100

    genreated new json with musicological data

--- scripts/src/mp3toarray.py
+++ scripts/src/mp3toarray.py
@@ -2,23 +2,52 @@ import os
 import scipy.io.wavfile as wav
 from pydub import AudioSegment
 import os
+import csv
 
 result = {}
 
-for file in os.listdir("../data/mp3s"):
-    if file.endswith(".mp3"):
-        fname = '../data/mp3s/' + file
+def read_CSV_file(path):
+    '''
+    function reads CSV file on path
+    and return the list with values from CSV
 
-        sound = AudioSegment.from_mp3(fname)
-        sound.export("temp.wav", format="wav")
+    parameters
+    ----------
+    path:string - path to CSV file
 
-        data = wav.read("temp.wav")
+    returns
+    ----------
+    list - list with csv values
+    '''
+    with open(path, 'rb') as f:
+        reader = csv.reader(f)
+        values = list(reader)
+    return values
 
-        print file
-        a = data[1][:,0].tolist() if data[1][1].size == 2 else data[1].tolist()
-        result[file] = a[0::len(a)/1000]
-        print len(result[file])
+if __name__ == "__main__":
 
-import json
-with open('songs.json', 'w') as fp:
-    json.dump(result, fp, indent=4)
+    for file in os.listdir("../data/mp3s"):
+        if file.endswith(".mp3"):
+            fname = '../data/mp3s/' + file
+
+            sound = AudioSegment.from_mp3(fname)
+            sound.export("temp.wav", format="wav")
+
+            data = wav.read("temp.wav")
+
+            print file
+            a = data[1][:,0].tolist() if data[1][1].size == 2 else data[1].tolist()
+            result[file] = {}
+            result[file]['sinusoide'] = a[0::len(a)/1000]
+
+    song_data = read_CSV_file('../data/template_pesmi_muzikologi.csv')
+    for i in range(2, len(song_data)):
+        for j in range(1, len(song_data[i])):
+            value = song_data[i][j]
+            key = song_data[0][j]
+            result[song_data[i][0]][key] = int(value) if j in [2, 3, 4, 5] else value 
+
+
+    import json
+    with open('songs.json', 'w') as fp:
+        json.dump(result, fp, indent=4)
