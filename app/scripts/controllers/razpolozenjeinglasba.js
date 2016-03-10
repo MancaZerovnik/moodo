aef9a6a4d4f73bac8011fa9e36517f564d3ec105
PrimozGodec <p.godec9@gmail.com>
2016-03-10 13:50:14 +0100

    corrections in metrum filter with fixed metrums without partial metrums and substing selection

--- app/scripts/controllers/razpolozenjeinglasba.js
+++ app/scripts/controllers/razpolozenjeinglasba.js
@@ -194,7 +194,7 @@ angular.module('modooApp')
         $scope.genres = ['Classical', 'Opera', 'Country', 'Folk', 'Latin', 'Dance/Disco', 'Electronic',
                        'R&B/Soul', 'Hip Hop/Rap', 'Reggae', 'Pop', 'Rock', 'Alternative', 'Metal',
                        'Blues', 'Jazz', 'Vocal', 'Easy Listening', 'New Age','Punk'];
-        $scope.metrums = getUniqueValuesListForAllSongs($scope.songsData, 'metrum');
+        $scope.metrums = ['2/4', '3/4', '4/4', '5/4', '7/4', '6/8', '9/8', '10/8', '12/8'];
         var minMax = bpmMinMax($scope.songsData);
         $scope.BPMmin = minMax[0];
         $scope.BPMmax = minMax[1];
