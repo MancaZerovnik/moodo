7a96ecda611115178a51f126494ca5855a7e8c18
PrimozGodec <p.godec9@gmail.com>
2016-04-05 22:14:26 +0200

    color sorting

--- app/scripts/controllers/razpolozenjeinglasba.js
+++ app/scripts/controllers/razpolozenjeinglasba.js
@@ -553,7 +553,11 @@ angular.module('modooApp')
                 data[group_idx].values[0].value = data[group_idx].values[0].value +1;
             }
         }
-        return data;
+        return _.sortBy(data, function(d){
+            var rgb = hexToRgb(d.key);
+            var hsv = rgb2hsv(rgb.r, rgb.g, rgb. b)
+            return 100*hsv.h + 10*hsv.s + hsv.v;
+        });
     }
 
     /*
@@ -654,4 +658,60 @@ angular.module('modooApp')
     function isNumeric(num){
         return !isNaN(num)
     }
+
+    function rgb2hsv () {
+        var rr, gg, bb,
+            r = arguments[0] / 255,
+            g = arguments[1] / 255,
+            b = arguments[2] / 255,
+            h, s,
+            v = Math.max(r, g, b),
+            diff = v - Math.min(r, g, b),
+            diffc = function(c){
+                return (v - c) / 6 / diff + 1 / 2;
+            };
+
+        if (diff == 0) {
+            h = s = 0;
+        } else {
+            s = diff / v;
+            rr = diffc(r);
+            gg = diffc(g);
+            bb = diffc(b);
+
+            if (r === v) {
+                h = bb - gg;
+            }else if (g === v) {
+                h = (1 / 3) + rr - bb;
+            }else if (b === v) {
+                h = (2 / 3) + gg - rr;
+            }
+            if (h < 0) {
+                h += 1;
+            }else if (h > 1) {
+                h -= 1;
+            }
+        }
+        return {
+            h: Math.round(h * 360),
+            s: Math.round(s * 100),
+            v: Math.round(v * 100)
+        };
+    }
+
+    function hexToRgb(hex) {
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        });
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
  });
